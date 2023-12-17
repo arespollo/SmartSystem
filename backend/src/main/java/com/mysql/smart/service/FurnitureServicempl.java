26529127600d1913c26f6eaf3c9e937cdb444ad6
hzt <3383976247@qq.com>
2023-12-17 14:58:39 +0800

    update: update project updatefur

--- backend/src/main/java/com/mysql/smart/service/FurnitureServicempl.java
+++ backend/src/main/java/com/mysql/smart/service/FurnitureServicempl.java
@@ -2,8 +2,16 @@ package com.mysql.smart.service;
 
 import com.mysql.smart.domain.Furniture;
 import com.mysql.smart.repository.FurnitureDao;
+import io.micrometer.common.util.StringUtils;
 import jakarta.annotation.Resource;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.stereotype.Service;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class FurnitureServicempl implements FurnitureService{
@@ -42,11 +50,27 @@ public class FurnitureServicempl implements FurnitureService{
 
         if (existingFurniture != null) {
 
+            BeanUtils.copyProperties(updatedFurniture, existingFurniture, getNullPropertyNames(updatedFurniture));
+
             return furnitureDao.save(existingFurniture);
         } else {
             // 如果找不到要更新的家具，可以抛出异常或返回null，具体根据需求而定
             return null;
         }
+    }
+    private String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for (PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null || StringUtils.isEmpty(srcValue.toString())) {
+                emptyNames.add(pd.getName());
+            }
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
     }
     /*@Override
     public User querySceneStatus(long id){
